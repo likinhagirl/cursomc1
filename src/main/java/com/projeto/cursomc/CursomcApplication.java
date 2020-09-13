237fe6997c64ed6e79809ee86a6a3cfa155900e4
likinhagirl <larissanunes121199@gmail.com>
2020-09-13 19:39:44 -0300

    projeto criado

---
+++ src/main/java/com/projeto/cursomc/CursomcApplication.java
@@ -0,0 +1,15 @@
+package com.projeto.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//anotação para iniciar aplicação
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
